7670a48c366e6d446a94c22112bfb92e16ce7683
Vladimir Shytsko <shytsko.v@gmail.com>
2022-05-09 12:49:51 +0300

    Add solution Vowel Count https://www.codewars.com/kata/54ff3102c1bad923760001f3

---
+++ VowelCount/Program.cs
@@ -0,0 +1,21 @@
+﻿// Return the number (count) of vowels in the given string.
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+// The input string will only consist of lower case letters and/or spaces.
+using System.Collections.Generic;
+
+
+Console.WriteLine(Kata.GetVowelCount("abracadabra"));
+
+public static class Kata
+{
+    public static int GetVowelCount(string str)
+    {
+        int vowelCount = 0;
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+        foreach (var item in str)
+            if(vowels.Contains(item))
+                vowelCount++;
+        return vowelCount;
+    }
+}
