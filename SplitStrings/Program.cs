75ae38cdb1365c85bfe39c6507d035166baa88b5
Vladimir Shytsko <shytsko.v@gmail.com>
2022-05-23 00:22:55 +0300

    Add solution Split Strings https://www.codewars.com/kata/515de9ae9dcfc28eb6000001

---
+++ SplitStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters
+// then it should replace the missing second character of the final pair with an underscore ('_').
+// Examples:
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+using System.Collections.Generic;
+
+Console.WriteLine(String.Join(" ", SplitString.Solution("abcdefk")));
+public class SplitString
+{
+    public static string[] Solution(string str)
+    {
+        List<string> result = new List<string>();
+        string temp = (str.Length % 2 == 0) ? str : str + '_';
+        for (int i = 0; i < temp.Length; i += 2)
+            result.Add(temp.Substring(i, 2));
+        return result.ToArray();
+    }
+}
