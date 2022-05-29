152fe907911b53f53752c5af5649bd2e4d2e91f5
Vladimir Shytsko <shytsko.v@gmail.com>
2022-05-29 23:20:36 +0300

    Add solution Detect Pangram https://www.codewars.com/kata/545cedaa9943f7fe7b000048

---
+++ DetectPangram/Program.cs
@@ -0,0 +1,23 @@
+﻿// A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence
+// "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+
+Console.WriteLine(Kata.IsPangram("The quik brown fox jumps over the lazy dog."));
+public static class Kata
+{
+    public static bool IsPangram(string str)
+    {
+        string letters = "abcdefghijklmnopqrstuvwxyz";
+
+        foreach (char letter in letters)
+        {
+            if(!str.Contains(letter) && !str.Contains($"{letter}".ToUpper()))
+            {
+                return false;
+            }
+        }
+        return true;
+        throw new NotImplementedException();
+    }
+}
