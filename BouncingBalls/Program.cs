0546c0f8a0f5fa901586b696e7d4a60259762b6e
Vladimir Shytsko <shytsko.v@gmail.com>
2022-05-30 00:05:21 +0300

    Add solution Bouncing Balls https://www.codewars.com/kata/5544c7a5cb454edb3c000047

---
+++ BouncingBalls/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine(BouncingBall.bouncingBall(3.0, 0.66, 1.5));
+Console.WriteLine(BouncingBall.bouncingBall(30.0, 0.66, 1.5));
+
+
+public class BouncingBall
+{
+
+    public static int bouncingBall(double h, double bounce, double window)
+    {
+        if (h > 0 && 0 < bounce && bounce < 1 && window < h)
+        {
+            int count = 1;
+            h *= bounce;
+            while (h > window)
+            {
+                count += 2;
+                h *= bounce;
+            }
+            return count;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
